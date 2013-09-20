086c82ebf8c28eaad2fabb7f8cf41e2e365d11f8
Yuri Costa <yuri@sparkroad.com>
2013-09-20 02:03:48 -0700

    all

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130920050343) do
+ActiveRecord::Schema.define(version: 20130920083326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,5 +49,14 @@ ActiveRecord::Schema.define(version: 20130920050343) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
+
+  create_table "waterings", force: true do |t|
+    t.datetime "time"
+    t.integer  "version"
+    t.integer  "channel"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
