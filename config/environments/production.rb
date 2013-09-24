9ee9249139448b4a4728e290fb1b8fb8f3731eac
Yuri Costa <yuri@sparkroad.com>
2013-09-24 04:17:25 -0700

    devise email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,17 @@ Waterall::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { :host => 'http://waterall.herokuapp.com/' }
+  
 end
+
+
+
+
+
+
+
+
+
+
+
