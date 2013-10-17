ce82465dbd460d506a7fa2d73dac8e2a5a1cfcde
Yuri Costa <yuri@sparkroad.com>
2013-10-17 02:52:38 -0700

    number validation for trigger

--- app/models/trigger.rb
+++ app/models/trigger.rb
@@ -1,6 +1,9 @@
 class Trigger < ActiveRecord::Base
+  validates :duration, :numericality => { :greater_than => 0 }
   belongs_to :channel
+  
   def to_formatted_watering(time)
     time.strftime("%H:%M")
   end
+  
 end
